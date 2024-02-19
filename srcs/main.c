5ad2b933667acc19ece88b360f61801b0a70e892
Iban Jean-Baptiste <ibjean-b@z3r4p2.42lyon.fr>
2024-02-19 16:56:07 +0100

    Parsing modified to let user change the value, need to add ft_atof fonction

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ibjean-b <ibjean-b@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:53:09 by ibjean-b          #+#    #+#             */
-/*   Updated: 2024/02/15 19:54:27 by ibjean-b         ###   ########.fr       */
+/*   Updated: 2024/02/19 16:54:17 by ibjean-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@
 void	error_exit(void)
 {
 	ft_printf\
-	("Available fractals are : 1 (Mandelbrot), 2 (Julia), 3 (Burning Ship)");
+	("\nAvailable fractals are : 1 (Mandelbrot), 2 (Julia), 3 (Burning Ship)\n\n");
+	ft_printf\
+	("Add up to 2 more arguments to modify the constant's value\n\n");
+	ft_printf\
+	("Ex : ./fractal 1 -0.2 0.333\n0.2 would be the imaginary part and 0.33 3 the real part.\n");
 	exit(0);
 }
 
@@ -32,6 +36,10 @@ void	parse_args(int argc, char **argv, t_vars *vars)
 			vars->params.name = "julia";
 		else if (!ft_strncmp(argv[1], "3", 2))
 			vars->params.name = "ship";
+		// if (argc >= 3)
+		// 	vars->params.c.z_r = ft_atof(argv[2]);
+		// if (argc == 4)
+		// 	vars->params.c.z_i = ft_atof(argv[2]);
 		else
 			error_exit();
 	}
